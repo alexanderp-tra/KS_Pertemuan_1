6ab9c12dbe7c74b85a5ba6819c4542ab79ffce21
alexanderp-tra <102359592+alexanderp-tra@users.noreply.github.com>
2022-03-26 11:36:28 +0700

    First

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/riwayatPendidikan', function () {
+    return view('riwayatPendidikan');
+});
+
+Route::get('/skill', function () {
+    return view('skill');
+});
+
+Route::get('/riwayatPekerjaan', function () {
+    return view('riwayatPekerjaan');
+});
+
+Route::get('/hobi', function () {
+    return view('hobi');
+});
+
+// Route::get('/Product/Produk1/namaproduk', function () {
+//     return view('kelompokA.product');
+// })->name('produksaya');
+
+// Route::get('/Service', function () {
+//     return view('kelompokA.service');
+// });
+
+// Route::get('/AboutUs', function () {
+//     return view('kelompokB.aboutus');
+// });
